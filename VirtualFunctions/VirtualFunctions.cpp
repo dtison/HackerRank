f37a61393e7ec3993af587e7239972ca89412da3
David Ison <developer@dtison.net>
2017-01-02 03:25:15 +0000

    Virtual Functions C++ solution passes all tests.

--- VirtualFunctions/VirtualFunctions.cpp
+++ VirtualFunctions/VirtualFunctions.cpp
@@ -5,76 +5,78 @@
 #include <algorithm>
 using namespace std;
 
+////  Challenge Code Begin 
 
-class Person{
-    protected:
-    string name;
-    int age;
+class Person {
     public:
-    
-    static int prof_id;
-    static int stud_id;
-    virtual void getdata()=0;
-    virtual void putdata()=0;
+        virtual void getdata() = 0;
+        virtual void putdata() = 0;
+    protected:
+        string name;
+        int age;
 };
 
-int Person::prof_id=0;
-int Person::stud_id=0;
 
-class Professor:public Person{
-    
-    int publications;
-    int cur_id;
-    
+class Professor: public Person {
+    static int next_id;
     public:
-     Professor(){
-        prof_id++;
-    }
-    void getdata();
-    void putdata();
-    
+        Professor() : id (next_id) {
+            next_id++;
+        }
+        void getdata();
+        void putdata();
+    protected:
+        int id;
+        int publications;
 };
 
 void Professor::getdata(){
-    cin>>name;
-    cin>>age;
-    cin>>publications;
-    cur_id=prof_id;
-    }
+    cin >> name;
+    cin >> age;
+    cin >> publications;
+}
 
 void Professor::putdata(){
-    cout<<name<<" "<<age<<" "<<publications<<" "<<cur_id<<endl;
-    }
+    cout << name << " " << age << " " << publications << " " << id <<endl;
+}
 
 
 class Student:public Person{
-    
-    int marks[6];
-    int cur_id;
+    static int next_id;
     public:
-    void getdata();
-    void putdata();
-    Student(){
-        stud_id++;
-    }   
+        void getdata();
+        void putdata();
+        Student() : id (next_id) {
+            next_id++;
+        } 
+    protected:
+        int id;
+        int marks[6];
 };
+
 void Student::getdata(){
-    cin>>name>>age;
-    for(int i=0;i<6;i++){
-        cin>>marks[i];
+    cin>> name >> age;
+    for (int i = 0; i < 6; i++){
+        cin >> marks[i];
     }
-    cur_id=stud_id;
 }
 
 void Student::putdata(){
     
     int total=0;
-    cout<<name<<" "<<age<<" ";
-    for(int j=0;j<6;j++){
-        total+=marks[j];
+    cout << name << " " << age << " ";
+    for (auto mark: marks) {
+        total += mark;
+
     }
-    cout<<total<<" "<<cur_id<<endl; 
+    cout << total << " " << id << endl; 
 }
+
+int Student::next_id = 1;
+int Professor::next_id = 1;
+
+////  Challenge Code End
+
 
 int main(){
 
@@ -82,10 +84,10 @@ int main(){
     cin>>n; //The number of objects that is going to be created.
     Person *per[n];
 
-    for(int i = 0;i < n;i++){
+    for (int i = 0;i < n;i++){
 
-        cin>>val;
-        if(val == 1){
+        cin >> val;
+        if (val == 1){
             // If val is 1 current object is of type Professor
             per[i] = new Professor;
 
