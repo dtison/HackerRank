00482ac8209cfbb1b315b9ff0d447ffaf79cd6f9
David Ison <developer@dtison.net>
2017-02-15 10:25:52 -0500

    Counting Sort 1 C++ solution passes all tests.

--- CountingSort1/CountingSort1.cpp
+++ CountingSort1/CountingSort1.cpp
@@ -17,40 +17,6 @@
 using namespace std;
 
 
-
-void displayArray(vector <int> & array) {
-    for_each(array.begin(), array.end(), [] (int & element) {cout << element << " ";});
-    cout << endl;
-}
-/*
-int partitionLomuto(vector <int> & numbers, int lo, int hi) {
-    int pivot = numbers[hi];
-    int i = lo - 1;
-    for (int j = lo; j < hi; j++) {
-        if (numbers[j] <= pivot) {
-            i++;
-            swap(numbers[i], numbers[j]);
-        }
-    }
-    swap(numbers[i + 1], numbers[hi]);
-
-    displayArray(numbers);
-
-    return i + 1;
-}
-
-void quickSort(vector <int> & numbers, int lo, int hi) {
-
-    // Continue sorting array until lo reaches hi
-    if (lo < hi) {
-        int pivot = partitionLomuto(numbers, lo, hi);
-        quickSort(numbers, lo, pivot - 1);
-        quickSort(numbers, pivot + 1, hi);
-    }
-}
-*/
-
-
 int main() {
     int n;
     cin >> n;
@@ -59,18 +25,14 @@ int main() {
     vector <int> numbers(n);
     for (int i = 0; i < n; i++) {
         int number;
-        cin >> number;
-        numbers[i] = number;
-        counts[number]++;
-        cout << "Count for value " << number << " is now "<< counts[number] << endl;
+        cin >> numbers[i];
+        counts[numbers[i]]++;
     }
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < 100; i++) {
         cout << counts[i] << " ";
     }
     cout << "\n";
-
-//    quickSort(numbers, 0, numbers.size() - 1);
 
     return 0;
 }
