c7c68641cac73f98053012f2ad0cdeca2d970095
David Ison <developer@dtison.net>
2017-02-15 10:09:28 -0500

    Partial CountingSort1

---
+++ CountingSort1/CountingSort1.cpp
@@ -0,0 +1,64 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+void displayArray(vector <int> & array) {
+    for_each(array.begin(), array.end(), [] (int & element) {cout << element << " ";});
+    cout << endl;
+}
+
+int partitionLomuto(vector <int> & numbers, int lo, int hi) {
+    int pivot = numbers[hi];
+    int i = lo - 1;
+    for (int j = lo; j < hi; j++) {
+        if (numbers[j] <= pivot) {
+            i++;
+            swap(numbers[i], numbers[j]);
+        }
+    }
+    swap(numbers[i + 1], numbers[hi]);
+
+    displayArray(numbers);
+
+    return i + 1;
+}
+
+void quickSort(vector <int> & numbers, int lo, int hi) {
+
+    // Continue sorting array until lo reaches hi
+    if (lo < hi) {
+        int pivot = partitionLomuto(numbers, lo, hi);
+        quickSort(numbers, lo, pivot - 1);
+        quickSort(numbers, pivot + 1, hi);
+    }
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector <int> numbers(n);
+    for (int i = 0; i < n; i++) {
+        cin >> numbers[i];
+    }
+
+    quickSort(numbers, 0, numbers.size() - 1);
+
+    return 0;
+}
---
+++ CountingSort1/README.md
@@ -0,0 +1,13 @@
+Quick Sort 3
+============
+
+C++ code [passes all tests](https://www.hackerrank.com/challenges/quicksort3) on 2017/02/14
+
+Original [Quick Sort 3](https://www.hackerrank.com/challenges/quicksort3) challenge.
+
+
+**Note about source comments:**
+
+This solution is intentionally written in an expressive and self-documenting style, so as to not need
+any source comments.
+
